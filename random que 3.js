f873eea4ff97c1813e7f9cf002246f217809157d
Afuriifsoddoma <143870416+Afuriifsoddoma@users.noreply.github.com>
2023-09-02 15:04:31 +0300

    Add files via upload

---
+++ random que 3.js
@@ -0,0 +1,540 @@
+const quiz =[
+    {
+        q:'Which month comes right before june ?',
+        options:['may','september','july','august'],
+        answer:0,
+        jokes:"beat/1beat.mp3"
+    },
+    {
+        q:'What color is a banana?',
+        options:['red','yellow','white','blue'],
+        answer:1,
+        jokes:"beat/diww.mp3"
+    },
+    {
+        q:'3 + 4 = 7 ?',
+        options:['true','false'],
+        answer:0,
+        jokes:"beat/diww.mp3"
+    },
+    {
+        q:' What time of day do we have breakfast ?',
+        options:['In the afternoon','In the evening','In the morning'],
+        answer:2
+    },
+    {
+        q:' What is 22 + 6',
+        options:['99','56','16','28'],
+        answer:3,
+        jokes:"beat/diww.mp3"
+    },
+    {
+        q:' tiriidantii oromoo',
+        options:['raachuu','abiyyi ahmad','lammaa','tsegaye'],
+        answer:0
+    },
+    {
+        q:' What is capital city of bale',
+        options:['robe','goba','ali','agerfa'],
+        answer:0
+    },
+    {
+        q:'irish singer',
+        options:['snead oconnor','dj khaled','alpha','burma boy'],
+        answer:0
+    },
+    {
+        q:' What is capital city of ethiopia',
+        options:['finfinnee','berara','addis ababa','shegar'],
+        answer:0
+    },
+    {
+        q:'ijoollee nuurhuseen',
+        options:['baalee','arsii','shawaa','wallaggaa'],
+        answer:0
+    },
+    {
+        q:'ijoollee leeqaa gamaa',
+        options:['wallagga','salaale','harargee','karrayyuu'],
+        answer:0
+    },
+    {
+        q:'best political analyst in the region',
+        options:['jaalannee maqaasaa','sisay agena','mesay mekonnin','habtaamuu ayyalew'],
+        answer:0
+    },
+    {
+        q:'june 29',
+        options:['Haacaaluu hundeessaa','usmaayyoo muussaa','yoosef gammachuu','ibraahim aadam'],
+        answer:0
+    },
+    {
+        q:'confuse and convince',
+        options:['shimallis abdiisaa','caaltuu saanii','awwaluu abdii','taayyee dendea'],
+        answer:0
+    },
+    {
+        q:'oromo protest irecha',
+        options:['gemeda wariyo','abdi roba','obsaa abdiisaa','kadiir martuu'],
+        answer:0
+    },
+    {
+        q:'ayyaana oromoo',
+        options:['caamsaa 20','irreecha','march 8','cambelaalaa'],
+        answer:1
+    },
+    {
+        q:'badhaadhina waliin fuuldurri keena',
+        options:['ifa','misooma','bilisummaa','koobilidha'],
+        answer:3
+    },
+    {
+        q:'Injifannoon kan ummata',
+        options:['oromooti','amaaraati','tigraayiti','safara keennaati'],
+        answer:0
+    },
+    {
+        q:'what is odd',
+        options:['12','11','90','16'],
+        answer:1
+    },
+    {
+        q:'chiggaaraamii oromoo',
+        options:['tsegaye merga','rachu tesfaye','jaalannee maqaasaa','nur ali'],
+        answer:0
+    },
+    {
+        q:'haasaadhaayi',
+        options:['ibsituu oromoo','monet gemechu','dani tamiru','alex taye'],
+        answer:0
+    },
+    {
+        q:' Marii sabaati',
+        options:['Jaal Marroo','abiyyi ahmad','birhanu naggaa','milishaa 01'],
+        answer:0
+    },
+    {
+        q:' kallacha qabsoo ummata oromooti',
+        options:['OPDO','ABO','KFO','seesee'],
+        answer:1
+    },
+    {
+        q:'maal jechuudha',
+        options:['taye dendea','shimelis abdisa','lammaa magarsaa','jawaar muhaammad'],
+        answer:0
+    },
+    {
+        q:'how many triangles are there in the image',
+        options:['12','7','13','9'],
+        answer:3,
+        img:'triangle.jpg',
+        imgFull:'irecha1.jpg'
+    },
+      {
+        q:'how many squares are there in the image',
+        options:['14','18','20','8'],
+        answer:1,
+        img:'square.jpg',
+        imgFull:'irecha1.jpg'
+    },
+    {
+            q:'who are these guys',
+            options:['ODAA','ADDIS ABABA','KOTEBEE','ALII'],
+            answer:0,
+            img:'sabaa.jpg',
+            imgFull:'irecha.jpg'
+    },
+    {
+        q:'who is this guy',
+        options:['Drake','justin bieber','pop smoke','dr dre'],
+        answer:0,
+        img:'drakes.jpg',
+        imgFull:'drake1.jpg'
+},
+{
+    q:'Guess this one',
+    options:['tsegaye','qeerroo','430','athlet'],
+    answer:0,
+    img1:'drake1.jpg',
+    img2:'drakes.jpg',
+    img3:'irecha.jpg',
+    img4:'irecha1.jpg'
+},
+{
+    q:'Their slogan',
+    options:['Oromia shall be free','Oromo builds Ethiopia','Oromo is come from madagascar','oromo must lead ethiopia'],
+    answer:0,
+    img1:'marroo.jpg',
+    img2:'marroo1.jpg',
+    img3:'hache.jpg',
+    img4:'hache1.jpg',
+    jokes:"beat/diww.mp3"
+},
+{
+    q:'Hint:Artist',
+    hard:'text',
+    options:[],
+    answer:["haacaaluu","Haacaaluu","HAACAALUU"],
+    placehold:"Maqaa",
+    images:[ "img6.jpg", "img6.jpg", "img6.jpg","img6.jpg", "irecha.jpg", "irecha1.jpg", "hache.jpg", "hache1.jpg"],
+    // jokes:"beat/jaal.mp3"
+    
+},
+{
+    q:'Hint: Guyyaa',
+    hard:'text',
+    options:[],
+    placehold:"Fulbaana 20",
+    answer:["EBLA 15","Ebla 15","ebla 15"],
+    images:["shinniggaa/img5.jpg","shinniggaa/img6.jpg","shinniggaa/img7.jpg","shinniggaa/img8.jpg","shinniggaa/img9.jpg","shinniggaa/img10.jpg","shinniggaa/img11.jpg","shinniggaa/img12.jpg"]   
+
+},
+ {
+    q:'Hint:place',
+    hard:'text',
+    options:[],
+    answer:["wancii","WANCII","Wancii"],
+    images:["wenchi/img5.jpg","wenchi/img6.jpg","wenchi/img7.jpg","wenchi/img8.jpg","wenchi/img9.jpg","wenchi/img10.jpg","wenchi/img11.jpg","wenchi/img12.jpg"],
+    audio:"beat/2beat.mp3"
+ },
+     
+{
+    q:'Hint:facebook username',
+    hard:'text',
+    options:[],
+    placehold:"Haasadhaayi",
+    answer:["ibsituu","Ibsituu","IBSITUU"],
+    images:["ibsituu/img5.png","ibsituu/img6.jpg","ibsituu/img7.jpg","ibsituu/img8.jpg","ibsituu/img9.jpg","ibsituu/img10.jpg","ibsituu/img11.jpg","ibsituu/img12.jpg"],
+    audio:"beat/beat5.mp3"
+    // img5:"ibsituu/img5.png",
+    // img6:"ibsituu/img6.jpg",
+    // img7:"ibsituu/img7.jpg",
+    // img8:"ibsituu/img8.jpg",
+    // img9:"ibsituu/img9.jpg",
+    // img10:"ibsituu/img10.jpg",
+    // img11:"ibsituu/img11.jpg",
+    // img12:"ibsituu/img12.jpg"
+},
+{
+    q:'Hint:Organization',
+    hard:'text',
+    options:[],
+    answer: ["ABO","OLF","WBO","430"],
+    images:[ "abo/img5.jpg", "abo/img6.jpg", "abo/img7.jpg", "abo/img8.jpg", "abo/img9.jpg", "abo/img10.jpg", "abo/img11.jpg", "abo/img12.jpg"],
+    audio:"beat/beat6.mp3"
+   
+},
+{
+    q:'Hint:Tiktok User Name',
+    hard:'text',
+    placehold:"abdaa",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["Abbaa Shittoo","ABBAA SHITTOO","abbaa shittoo","Abbaa shittoo"],
+    images:[ "shitto/img5.jpg", "shitto/img6.jpg", "shitto/img7.jpg", "shitto/img8.jpg", "shitto/img9.jpg", "shitto/img10.jpg", "shitto/img11.jpg", "shitto/img12.jpeg"],
+    audio:"beat/beat7.mp3"
+   
+},
+{
+    q:'Hint:Facebook User Name',
+    hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sabboontuu","SABBOONTUU","Sabboontuu","sebontu"],
+    images:[ "sab/img5.jpg", "sab/img6.jpg", "sab/img7.jpg", "sab/img8.jpg", "sab/img9.jpg", "sab/img10.jpg", "sab/img11.jpg", "sab/img12.jpg"],
+    audio:"beat/beat5.mp3"
+   
+},
+{
+    q:'Hint:Facebook User Name',
+    hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+ images:[ "aagaa/img5.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    audio:"beat/beat6.mp3",
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img5.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/beat6.mp3",
+    musicA:["ALII BIRRAA","Alii Birraa","alii birraa"]
+
+   
+   
+},
+// {
+//     q:'Hint:Facebook User Name',
+//     // hard:'text',
+//     // placehold:"username",
+//     // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+//     options:[],
+//     answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+//     images:[ "aagaa/img5.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+//     audio:"beat/beat6.mp3",
+//     pics4:["a","b","c"],
+//     deebii:"sab aga",
+//     dirq:["S","A","B","G","Z","H","T"]
+   
+   
+// },
+{
+    q:'',
+    hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["Afran Qalloo","afran qalloo","AFRAN QALLOO","Afran Qallo"],
+ images:[ "afran/img5.jpg", "afran/img6.jpg", "afran/img7.jpg", "afran/img8.jpg", "afran/img9.jpg", "afran/img10.jpg", "afran/img11.jpg", "afran/img12.jpg"],
+    audio:"beat/beat4.mp3",
+   
+   
+},
+{
+    q:'',
+    hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["Siifan Hasan","siifan","siifan hasan","Siifan"],
+    images:[ "sifan/img5.jpg", "sifan/img6.jpg", "sifan/img7.jpg", "sifan/img8.jpg", "sifan/img9.jpg", "sifan/img10.jpg", "sifan/img11.jpg", "sifan/img12.jpg"],
+    audio:"beat/beat6.mp3",
+   
+   
+},
+{
+    q:'',
+    hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["Saamii Karrayyuu","SAAMII KARRAYYUU","saamii karrayyuu","saamii"],
+    images:[ "saamii/img5.jpg", "saamii/img6.jpg", "saamii/img7.jpg", "saamii/img8.jpg", "saamii/img9.jpg", "saamii/img10.jpg", "saamii/img11.jpg", "saamii/img12.jpg"],
+    audio:"beat/umar.mp3",
+   
+   
+},
+{
+    q:'',
+    hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["Karra Hayyuu","KARRAYYUU","Karrayyuu","karrayyuu"],
+    images:[ "kerayu/img5.jpg", "kerayu/img6.jpg", "kerayu/img7.jpg", "kerayu/img8.jpg", "kerayu/img9.jpg", "kerayu/img10.jpg", "kerayu/img11.jpg", "kerayu/img12.jpg"],
+    audio:"beat/umar.mp3",
+   
+   
+},
+{
+    q:'',
+    hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["Alex Taye","ALEX TAYE","alex taye","Alex"],
+    images:[ "alex/img5.jpg", "alex/img6.jpg", "alex/img7.jpg", "alex/img8.jpg", "alex/img9.jpg", "alex/img10.jpg", "alex/img11.jpg", "alex/img12.jpg"],
+    audio:"beat/yanet.mp3",
+   
+   
+},
+{
+    q:'',
+    hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["Moggoo","moggoo","MOGGOO","moggoo"],
+    images:[ "moggoo/img5.jpg", "moggoo/img6.jpg", "moggoo/img7.jpg", "moggoo/img8.jpg", "moggoo/img9.jpg", "moggoo/img10.jpg", "moggoo/img11.jpg", "moggoo/img12.jpg"],
+    audio:"beat/beat6.mp3",
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img5.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/aadam.mp3",
+    musicA:["Aadam Haarun","AADAM HAARUN","aadam haarun"]
+
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img9.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/abdi.mp3",
+    musicA:["Abdii Nuuressaa","ABDII NUURESSAA","abdii nuuressaa"]
+
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img11.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/abeba.mp3",
+    musicA:["Abbabaa Abbishuu","ABBABAA ABBISHUU","abbabaa abbishuu"]
+
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img5.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/addisuu.mp3",
+    musicA:["Addisuu Furgaasaa","ADDISUU FURGAASAA","addisuu furgaasaa"]
+
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img5.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/adnan.mp3",
+    musicA:["Adnaan Muhaammad","ADNAAN MUHAAMMAD","adnaan muhaammad"]
+
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img5.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/alii.mp3",
+    musicA:["ALII BIRRAA","Alii Birraa","alii birraa"]
+
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img5.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/aliyi.mp3",
+    musicA:["Aliyyii Saabit","ALIYYII SAABIT","aliyyii saabit"]
+
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img5.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/awash.mp3",
+    musicA:["ALII BIRRAA","Alii Birraa","alii birraa"]
+
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img9.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/dawite.mp3",
+    musicA:["Daawitee Mokonnin","Daawit Mokonnin","DAAWITEE MOKONNIN"]
+
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img7.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/galataa.mp3",
+    musicA:["Galataa Labuu","GALATAA LABUU","galataa labuu"]
+
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img9.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/girmaa.mp3",
+    musicA:["Girmaa Xurunah","girmaa xurunah","GIRMAA XURUNAAH"]
+
+   
+   
+},
+{
+    q:' Artist Full Name',
+    // hard:'text',
+    placehold:"username",
+    // funny:"abdaa budaan nyaatte maqaa kiyya wallaaltee",
+    options:[],
+    answer: ["sab aagaa","sab aga","SAB AAGAA","SAB AGA"],
+    images:[ "aagaa/img7.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"],
+    music:"beat/sabboonaa.mp3",
+    musicA:["Sabboonaa Tafarraa","SABBOONAA TAFARRAA","sabboonaa tafarraa"]
+
+   
+   
+},
+
+
+
+ 
+
+]
+const Videosrc=["video.mp4","gobez.mp4","gobez1.mp4","gobez2.mp4","gobez3.mp4"];
+const duration1=[59000,15000,27000,21000,14000];
+const Videosrc2=["lazy.mp4","Lazy1.mp4","Lazy2.mp4","Lazy3.mp4","Lazy4.mp4","Lazy5.mp4","Lazy6.mp4","Lazy7.mp4"];
+const duration=[33000,35000,50000,32000,8000,8000,17000,19000];
+const bodyImg=[ "aagaa/img5.jpg", "aagaa/img6.jpg", "aagaa/img7.jpg", "aagaa/img8.jpg", "aagaa/img9.jpg", "aagaa/img10.jpg", "aagaa/img11.jpg", "aagaa/img12.jpg"]
